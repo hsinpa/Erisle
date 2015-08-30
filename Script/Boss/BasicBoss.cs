ce871c9f73a84f86333869258b1fb6610586ef42
HsiN <hsinpa@gmail.com>
2015-08-30 22:39:24 +0800

    update

---
+++ Script/Boss/BasicBoss.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Boss {
+public class BasicBoss : MoveClass {
+	public float hp = 100;
+	public Animator anim;
+	public SkillState currentState;
+	public int maxWalkVelocity = 3;
+	public int maxAttackVelocity = 7;
+	public int slowRadius = 8;
+	public float spawnRange = 15;
+	public int searchRadius = 6;
+	public int playerLayer = 1 << 10;
+	public int terrainLayer = 1 << 8;
+	public Transform target;
+	public Vector3 centerPoint;
+		
+	public void Start() {
+		anim = gameObject.GetComponent<Animator>();
+		centerPoint = transform.position;
+	}
+	
+	public void FixedUpdate () {
+		calculateGrivaty();
+	}
+	
+	public void changeState(SkillState newState) {
+		currentState.exit();
+		currentState = newState;
+		newState.enter(this);
+	}
+		
+	}
+}
