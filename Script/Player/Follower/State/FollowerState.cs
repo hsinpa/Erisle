ef3547778192801f44e15fc327458e911571aa90
Hsinpa <hsinpa@gmail.com>
2015-09-08 17:11:19 +0800

    update

---
+++ Script/Player/Follower/State/FollowerState.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Follower {
+	public interface FollowerState {
+
+		void execute();
+		void enter(FollowerManager follower);
+		void exit();
+	}
+}
